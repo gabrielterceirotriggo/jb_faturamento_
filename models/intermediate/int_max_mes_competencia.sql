5cd6d3ec2ecf4c4df371104789c163408d7f44e8
Gabriel Terceiro <gcarolinoterceiro@gmail.com>
2025-06-07 00:18:49 -0300

    Refactor: Improve int_max_mes_competencia readability
    Breaks down the query into CTEs for clarity and maintainability.
    This enhances understanding of each step in calculating max_mes.

--- models/intermediate/int_max_mes_competencia.sql
+++ models/intermediate/int_max_mes_competencia.sql
@@ -1,3 +1,22 @@
-select MAX(mes_competencia) as max_mes
-from
-    {{ ref('itens_faturamento_delta') }}
+with itens_faturamento_delta as (
+    select
+        mes_competencia
+    from
+        {{ ref('itens_faturamento_delta') }}
+),
+
+calculo_mes_maximo as (
+    select
+        MAX(mes_competencia) as max_mes
+    from
+        itens_faturamento_delta
+),
+
+final as (
+    select
+        max_mes
+    from
+        calculo_mes_maximo
+)
+
+select * from final
