598414d9d24b6794fa89cbc5a6dbb0b191f1ee57
Gabriel Terceiro <gcarolinoterceiro@gmail.com>
2025-06-06 21:30:53 -0300

    Refactor estornos_plenos_delta model
    Introduces CTEs for improved readability and organization of the
    estornos_plenos_delta model. No functional change intended.

--- models/intermediate/estornos_plenos_delta.sql
+++ models/intermediate/estornos_plenos_delta.sql
@@ -1,22 +1,71 @@
-select
-    documento_impressao,
-    motivo_criacao_impressao,
-    tipo_impressao,
-    data_criacao_impressao,
-    usuario_criacao,
-    data_modificacao_impressao,
-    usuario_modificacao,
-    data_competencia,
-    data_vencimento_original,
-    data_apresentacao,
-    chave_reconciliacao,
-    motivo_estorno_impressao,
-    contrapartida,
-    valor_total*-1 as valor_total,
-    estorno_pleno,
-    fatura_virtual,
-    TO_VARCHAR(data_competencia, 'YYYYMM') as mes_competencia
-from
-    {{ ref ('int_impressao_delta') }}
-where
-    estorno_pleno = 'X'
+with impressao_delta as (
+    select
+        documento_impressao,
+        motivo_criacao_impressao,
+        tipo_impressao,
+        data_criacao_impressao,
+        usuario_criacao,
+        data_modificacao_impressao,
+        usuario_modificacao,
+        data_competencia,
+        data_vencimento_original,
+        data_apresentacao,
+        chave_reconciliacao,
+        motivo_estorno_impressao,
+        contrapartida,
+        valor_total,
+        estorno_pleno,
+        fatura_virtual
+    from
+        {{ ref ('int_impressao_delta') }}
+),
+
+estornos_plenos_transformados as (
+    select
+        documento_impressao,
+        motivo_criacao_impressao,
+        tipo_impressao,
+        data_criacao_impressao,
+        usuario_criacao,
+        data_modificacao_impressao,
+        usuario_modificacao,
+        data_competencia,
+        data_vencimento_original,
+        data_apresentacao,
+        chave_reconciliacao,
+        motivo_estorno_impressao,
+        contrapartida,
+        valor_total * -1 as valor_total,
+        estorno_pleno,
+        fatura_virtual,
+        TO_VARCHAR(data_competencia, 'YYYYMM') as mes_competencia
+    from
+        impressao_delta
+    where
+        estorno_pleno = 'X'
+),
+
+final as (
+    select
+        documento_impressao,
+        motivo_criacao_impressao,
+        tipo_impressao,
+        data_criacao_impressao,
+        usuario_criacao,
+        data_modificacao_impressao,
+        usuario_modificacao,
+        data_competencia,
+        data_vencimento_original,
+        data_apresentacao,
+        chave_reconciliacao,
+        motivo_estorno_impressao,
+        contrapartida,
+        valor_total,
+        estorno_pleno,
+        fatura_virtual,
+        mes_competencia
+    from
+        estornos_plenos_transformados
+)
+
+select * from final
