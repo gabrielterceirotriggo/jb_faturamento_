1e7216076dbe1b606a03fcf3fc1f1ee8d9a4cf03
Gabriel Terceiro <gcarolinoterceiro@gmail.com>
2025-06-06 21:35:04 -0300

    Refactor: Improve faturamento_delta model
    Unifies intermediate models, transforms and final selects.
    Improves readability and simplifies data flow.

--- models/intermediate/faturamento_delta.sql
+++ models/intermediate/faturamento_delta.sql
@@ -1,4 +1,4 @@
-with int_consumo_delta as (
+with consumo_delta as (
     select
         mes_competencia,
         documento_calculo,
@@ -29,7 +29,7 @@ with int_consumo_delta as (
         {{ ref ('int_consumo_delta') }}
 ),
 
-int_calculo_delta as (
+calculo_delta as (
     select
         mes_competencia,
         mes_referencia,
@@ -87,153 +87,320 @@ int_calculo_delta as (
         {{ ref ('int_calculo_delta') }}
 ),
 
-int_consumo_registrado as (
+consumo_registrado as (
     select
         mes_competencia,
         documento_calculo,
-        consumo_registrado,
-        documento_impressao
+        documento_impressao,
+        consumo_registrado
     from
         {{ ref ('int_consumo_registrado') }}
+),
+
+dados_faturamento_unificados as (
+    select
+        icd.mes_competencia,
+        icd.mes_referencia,
+        icd.documento_calculo,
+        icd.documento_impressao,
+        icd.fatura,
+        icd.instalacao,
+        icd.conta_contrato,
+        icd.parceiro_negocio,
+        icd.contrato,
+        icd.unidade_leitura,
+        icd.etapa,
+        csd.setor_industrial,
+        csd.categoria_tarifa,
+        csd.subclasse,
+        icd.motivo_criacao_impressao,
+        icd.tipo_calculo,
+        icd.origem_documento,
+        icd.cnr,
+        icd.estornado,
+        icd.estorno_pleno,
+        icd.estorno_ajuste,
+        icd.reversao,
+        icd.cancelamento,
+        icd.fatura_virtual,
+        icd.minimo,
+        icd.documento_estorno_pleno,
+        icd.data_estorno_pleno,
+        icd.motivo_estorno_pleno,
+        icd.documento_estorno_ajuste,
+        icd.data_estorno_ajuste,
+        icd.motivo_estorno_ajuste,
+        icd.valor_fatura,
+        icd.valor_contabil,
+        csd.consumo_faturado,
+        csd.consumo_medido,
+        csd.eusd,
+        csd.eusdb,
+        csd.icms,
+        csd.icms_subvencao,
+        csd.pis,
+        csd.cofins,
+        csd.cip,
+        csd.retencao,
+        csd.receita_bandeiras,
+        csd.receita_consumo_faturado,
+        csd.tarifa,
+        csd.correcao_monetaria,
+        csd.creditos,
+        csd.estornos,
+        csd.juros,
+        csd.multas,
+        csd.parcelamentos,
+        icd.chave_reconciliacao,
+        icd.domicilio_fiscal,
+        icd.inicio_calculo,
+        icd.fim_calculo,
+        icd.quantidade_dias,
+        icd.data_competencia,
+        icd.data_atribuicao_calculo,
+        icd.data_apresentacao,
+        icd.data_vencimento_original,
+        icd.data_previsao_leitura,
+        icd.data_criacao_impressao,
+        icd.usuario_criacao_impressao,
+        icd.data_modificacao_impressao,
+        icd.usuario_modificacao_impressao,
+        icd.data_criacao_calculo,
+        icd.usuario_criacao_calculo,
+        icd.data_modificacao_calculo,
+        icd.usuario_modificacao_calculo,
+        icd.documento_calculo_anterior,
+        icd.estrutura_regional_politica,
+        csr.consumo_registrado,
+        icd.tipo_impressao as tipo_impressao_original,
+        icd.formulario_pagamento as formulario_pagamento_original
+    from
+        calculo_delta icd
+    inner join
+        consumo_delta csd
+        on
+            icd.mes_competencia = csd.mes_competencia
+            and icd.documento_calculo = csd.documento_calculo
+            and icd.documento_impressao = csd.documento_impressao
+    left outer join
+        consumo_registrado csr
+        on
+            icd.mes_competencia = csr.mes_competencia
+            and icd.documento_calculo = csr.documento_calculo
+            and icd.documento_impressao = csr.documento_impressao
+),
+
+faturamento_transformado as (
+    select
+        mes_competencia,
+        mes_referencia,
+        documento_calculo,
+        documento_impressao,
+        fatura,
+        instalacao,
+        conta_contrato,
+        parceiro_negocio,
+        contrato,
+        unidade_leitura,
+        etapa,
+        setor_industrial,
+        categoria_tarifa,
+        subclasse,
+        motivo_criacao_impressao,
+        tipo_calculo,
+        origem_documento,
+        cnr,
+        estornado,
+        estorno_pleno,
+        estorno_ajuste,
+        reversao,
+        cancelamento,
+        fatura_virtual,
+        minimo,
+        documento_estorno_pleno,
+        data_estorno_pleno,
+        motivo_estorno_pleno,
+        documento_estorno_ajuste,
+        data_estorno_ajuste,
+        motivo_estorno_ajuste,
+        valor_fatura,
+        valor_contabil,
+        consumo_faturado,
+        consumo_medido,
+        eusd,
+        eusdb,
+        icms,
+        icms_subvencao,
+        pis,
+        cofins,
+        cip,
+        retencao,
+        receita_bandeiras,
+        receita_consumo_faturado,
+        tarifa,
+        correcao_monetaria,
+        creditos,
+        estornos,
+        juros,
+        multas,
+        parcelamentos,
+        chave_reconciliacao,
+        domicilio_fiscal,
+        inicio_calculo,
+        fim_calculo,
+        quantidade_dias,
+        data_competencia,
+        data_atribuicao_calculo,
+        data_apresentacao,
+        data_vencimento_original,
+        data_previsao_leitura,
+        data_criacao_impressao,
+        usuario_criacao_impressao,
+        data_modificacao_impressao,
+        usuario_modificacao_impressao,
+        data_criacao_calculo,
+        usuario_criacao_calculo,
+        data_modificacao_calculo,
+        usuario_modificacao_calculo,
+        documento_calculo_anterior,
+        estrutura_regional_politica,
+        null as ordem_faturamento,
+        consumo_registrado,
+        SUBSTR(categoria_tarifa, 1, 1) as grupo,
+        case
+            when
+                categoria_tarifa in (
+                    'A1_LVAZ',
+                    'A1_LVVD',
+                    'A2_LVAZ',
+                    'A2_LVVD',
+                    'AÇA_LVAZ',
+                    'A3A_LVVD',
+                    'A3_LVAZ',
+                    'A3_LVVD',
+                    'A4_LVAZ',
+                    'A4_LVVD'
+                )
+                then 'X'
+        end as cliente_livre,
+        case
+            when tipo_impressao_original = ' '
+                then null
+            else tipo_impressao_original
+        end as tipo_impressao,
+        case
+            when COALESCE(consumo_faturado, 0) = 0
+                then null
+            else
+                COALESCE(receita_consumo_faturado, 0)
+                / consumo_faturado
+        end as preco,
+        COALESCE(valor_fatura, 0)
+        - COALESCE(receita_consumo_faturado, 0)
+        - COALESCE(receita_bandeiras, 0)
+        - COALESCE(cip, 0)
+        - COALESCE(correcao_monetaria, 0)
+        - COALESCE(creditos, 0)
+        - COALESCE(estornos, 0)
+        - COALESCE(juros, 0)
+        - COALESCE(multas, 0)
+        - COALESCE(parcelamentos, 0)
+        - COALESCE(icms_subvencao, 0) as outros_lancamentos,
+        case
+            when formulario_pagamento_original = ' '
+                then null
+            else formulario_pagamento_original
+        end as formulario_pagamento,
+        SYSDATE() as data_dados
+    from
+        dados_faturamento_unificados
+),
+
+final as (
+    select
+        mes_competencia,
+        mes_referencia,
+        documento_calculo,
+        documento_impressao,
+        fatura,
+        instalacao,
+        conta_contrato,
+        parceiro_negocio,
+        contrato,
+        unidade_leitura,
+        etapa,
+        setor_industrial,
+        categoria_tarifa,
+        subclasse,
+        motivo_criacao_impressao,
+        tipo_calculo,
+        origem_documento,
+        cnr,
+        estornado,
+        estorno_pleno,
+        estorno_ajuste,
+        reversao,
+        cancelamento,
+        fatura_virtual,
+        minimo,
+        documento_estorno_pleno,
+        data_estorno_pleno,
+        motivo_estorno_pleno,
+        documento_estorno_ajuste,
+        data_estorno_ajuste,
+        motivo_estorno_ajuste,
+        valor_fatura,
+        valor_contabil,
+        consumo_faturado,
+        consumo_medido,
+        eusd,
+        eusdb,
+        icms,
+        icms_subvencao,
+        pis,
+        cofins,
+        cip,
+        retencao,
+        receita_bandeiras,
+        receita_consumo_faturado,
+        tarifa,
+        correcao_monetaria,
+        creditos,
+        estornos,
+        juros,
+        multas,
+        parcelamentos,
+        chave_reconciliacao,
+        domicilio_fiscal,
+        inicio_calculo,
+        fim_calculo,
+        quantidade_dias,
+        data_competencia,
+        data_atribuicao_calculo,
+        data_apresentacao,
+        data_vencimento_original,
+        data_previsao_leitura,
+        data_criacao_impressao,
+        usuario_criacao_impressao,
+        data_modificacao_impressao,
+        usuario_modificacao_impressao,
+        data_criacao_calculo,
+        usuario_criacao_calculo,
+        data_modificacao_calculo,
+        usuario_modificacao_calculo,
+        documento_calculo_anterior,
+        estrutura_regional_politica,
+        ordem_faturamento,
+        consumo_registrado,
+        grupo,
+        cliente_livre,
+        tipo_impressao,
+        preco,
+        outros_lancamentos,
+        formulario_pagamento,
+        data_dados
+    from
+        faturamento_transformado
 )
 
-select
-    int_calculo_delta.mes_competencia,
-    int_calculo_delta.mes_referencia,
-    int_calculo_delta.documento_calculo,
-    int_calculo_delta.documento_impressao,
-    int_calculo_delta.fatura,
-    int_calculo_delta.instalacao,
-    int_calculo_delta.conta_contrato,
-    int_calculo_delta.parceiro_negocio,
-    int_calculo_delta.contrato,
-    int_calculo_delta.unidade_leitura,
-    int_calculo_delta.etapa,
-    int_consumo_delta.setor_industrial,
-    int_consumo_delta.categoria_tarifa,
-    int_consumo_delta.subclasse,
-    int_calculo_delta.motivo_criacao_impressao,
-    int_calculo_delta.tipo_calculo,
-    int_calculo_delta.origem_documento,
-    int_calculo_delta.cnr,
-    int_calculo_delta.estornado,
-    int_calculo_delta.estorno_pleno,
-    int_calculo_delta.estorno_ajuste,
-    int_calculo_delta.reversao,
-    int_calculo_delta.cancelamento,
-    int_calculo_delta.fatura_virtual,
-    int_calculo_delta.minimo,
-    int_calculo_delta.documento_estorno_pleno,
-    int_calculo_delta.data_estorno_pleno,
-    int_calculo_delta.motivo_estorno_pleno,
-    int_calculo_delta.documento_estorno_ajuste,
-    int_calculo_delta.data_estorno_ajuste,
-    int_calculo_delta.motivo_estorno_ajuste,
-    int_calculo_delta.valor_fatura,
-    int_calculo_delta.valor_contabil,
-    int_consumo_delta.consumo_faturado,
-    int_consumo_delta.consumo_medido,
-    int_consumo_delta.eusd,
-    int_consumo_delta.eusdb,
-    int_consumo_delta.icms,
-    int_consumo_delta.icms_subvencao,
-    int_consumo_delta.pis,
-    int_consumo_delta.cofins,
-    int_consumo_delta.cip,
-    int_consumo_delta.retencao,
-    int_consumo_delta.receita_bandeiras,
-    int_consumo_delta.receita_consumo_faturado,
-    int_consumo_delta.tarifa,
-    int_consumo_delta.correcao_monetaria,
-    int_consumo_delta.creditos,
-    int_consumo_delta.estornos,
-    int_consumo_delta.juros,
-    int_consumo_delta.multas,
-    int_consumo_delta.parcelamentos,
-    int_calculo_delta.chave_reconciliacao,
-    int_calculo_delta.domicilio_fiscal,
-    int_calculo_delta.inicio_calculo,
-    int_calculo_delta.fim_calculo,
-    int_calculo_delta.quantidade_dias,
-    int_calculo_delta.data_competencia,
-    int_calculo_delta.data_atribuicao_calculo,
-    int_calculo_delta.data_apresentacao,
-    int_calculo_delta.data_vencimento_original,
-    int_calculo_delta.data_previsao_leitura,
-    int_calculo_delta.data_criacao_impressao,
-    int_calculo_delta.usuario_criacao_impressao,
-    int_calculo_delta.data_modificacao_impressao,
-    int_calculo_delta.usuario_modificacao_impressao,
-    int_calculo_delta.data_criacao_calculo,
-    int_calculo_delta.usuario_criacao_calculo,
-    int_calculo_delta.data_modificacao_calculo,
-    int_calculo_delta.usuario_modificacao_calculo,
-    int_calculo_delta.documento_calculo_anterior,
-    int_calculo_delta.estrutura_regional_politica,
-    null as ordem_faturamento,
-    int_consumo_registrado.consumo_registrado,
-    SUBSTR(int_consumo_delta.categoria_tarifa, 1, 1) as grupo,
-    case
-        when
-            int_consumo_delta.categoria_tarifa in (
-                'A1_LVAZ',
-                'A1_LVVD',
-                'A2_LVAZ',
-                'A2_LVVD',
-                'AÇA_LVAZ',
-                'A3A_LVVD',
-                'A3_LVAZ',
-                'A3_LVVD',
-                'A4_LVAZ',
-                'A4_LVVD'
-            )
-            then 'X'
-    end as cliente_livre,
-    case
-        when int_calculo_delta.tipo_impressao = ' '
-            then null
-        else int_calculo_delta.tipo_impressao
-    end as tipo_impressao,
-    case
-        when COALESCE(int_consumo_delta.consumo_faturado, 0) = 0
-            then null
-        else
-            COALESCE(int_consumo_delta.receita_consumo_faturado, 0)
-            / int_consumo_delta.consumo_faturado
-    end as preco,
-    COALESCE(int_calculo_delta.valor_fatura, 0)
-    - COALESCE(int_consumo_delta.receita_consumo_faturado, 0)
-    - COALESCE(int_consumo_delta.receita_bandeiras, 0)
-    - COALESCE(int_consumo_delta.cip, 0)
-    - COALESCE(int_consumo_delta.correcao_monetaria, 0)
-    - COALESCE(int_consumo_delta.creditos, 0)
-    - COALESCE(int_consumo_delta.estornos, 0)
-    - COALESCE(int_consumo_delta.juros, 0)
-    - COALESCE(int_consumo_delta.multas, 0)
-    - COALESCE(int_consumo_delta.parcelamentos, 0)
-    - COALESCE(int_consumo_delta.icms_subvencao, 0) as outros_lancamentos,
-    case
-        when int_calculo_delta.formulario_pagamento = ' '
-            then null
-        else int_calculo_delta.formulario_pagamento
-    end as formulario_pagamento,
-    SYSDATE() as data_dados
-from
-    int_calculo_delta
-inner join
-    int_consumo_delta
-    on
-        int_calculo_delta.mes_competencia = int_consumo_delta.mes_competencia
-        and int_calculo_delta.documento_calculo
-        = int_consumo_delta.documento_calculo
-        and int_calculo_delta.documento_impressao
-        = int_consumo_delta.documento_impressao
-left outer join
-    int_consumo_registrado
-    on
-        int_calculo_delta.mes_competencia
-        = int_consumo_registrado.mes_competencia
-        and int_calculo_delta.documento_calculo
-        = int_consumo_registrado.documento_calculo
-        and int_calculo_delta.documento_impressao
-        = int_consumo_registrado.documento_impressao
+select * from final
