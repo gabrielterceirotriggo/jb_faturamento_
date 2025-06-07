6b51f913f1f9d65e0c35db6f9216bae8b47a612a
Gabriel Terceiro <gcarolinoterceiro@gmail.com>
2025-06-07 00:07:54 -0300

    Refactor: Improve readability of int_itens_extras_receita
    Breaks down CTEs for better understanding of data flow and
    improves overall code clarity. No functional changes.

--- models/intermediate/int_itens_extras_receita.sql
+++ models/intermediate/int_itens_extras_receita.sql
@@ -1,63 +1,143 @@
-select
-    int_documentos_faturamento.mes_competencia,
-    int_documentos_faturamento.doc_calculo as documento_calculo,
-    int_documentos_faturamento.doc_impressao as documento_impressao,
-    null as linha,
-    null as setor_industrial,
-    null as categoria_tarifa,
-    null as subclasse,
-    {# int_documentos_faturamento.tipo_calculo,
-    int_documentos_faturamento.tipo_documento, #}
-    dberdl.belzart as item_documento,
-    '000' as escalao,
-    dberdl.sbasw as base_imposto,
-    'D' as flag,
-    int_documentos_faturamento.estorno,
-    case
-        when dberdl.linesort = ' ' then null
-        else dberdl.linesort
-    end as item_ordenacao,
-    case
-        when dberdl.ab <> '00000000' then TO_DATE(dberdl.ab, 'YYYYMMDD')
-    end as inicio_calculo,
-    case
-        when dberdl.bis <> '00000000' then TO_DATE(dberdl.bis, 'YYYYMMDD')
-    end as fim_calculo,
-    case
-        when dberdl.ktosl = ' ' then null
-        else dberdl.ktosl
-    end as tipo_imposto,
-    CAST(null as NUMBER) as consumo,
-    CAST(null as NUMBER) as preco,
-    case
-        when int_documentos_faturamento.estorno = 'x' then dberdl.nettobtr * -1
-        else dberdl.nettobtr
-    end as receita,
-    case
-        when dberdl.stprz = ' ' then 0
-        else TRY_CAST(LTRIM(dberdl.stprz, ' ') as NUMBER(10, 3)) / 1000
-    end as aliquota,
-    case
-        when dberdl.hvorg = ' ' then null
-        else dberdl.hvorg
-    end as operacao,
-    case
-        when dberdl.tvorg = ' ' then null
-        else dberdl.tvorg
-    end as sub_operacao,
-    case
-        when dberdl.txjcd = ' ' then null
-        else dberdl.txjcd
-    end as domicilio_fiscal
-from
-    {{ ref('int_documentos_faturamento') }} as int_documentos_faturamento
-left outer join
-    {{ ref('stg_dberdl') }} as dberdl
-    on
-        int_documentos_faturamento.doc_impressao = dberdl.printdoc
-where
-    dberdl.xtotal_amnt <> 'X'
-    and dberdl.linesort in (
-        'ZDR1', 'ZDR2', 'ZDR3', 'ZDR4', 'ZDR5',
-        'ZDR6', 'ZUR1', 'ZUR2', 'ZUR3', 'ZDES'
-    )
+with documentos_faturamento as (
+    select
+        mes_competencia,
+        doc_calculo,
+        doc_impressao,
+        estorno
+    from
+        {{ ref('int_documentos_faturamento') }}
+),
+
+dberdl as (
+    select
+        printdoc,
+        belzart,
+        sbasw,
+        linesort,
+        ab,
+        bis,
+        ktosl,
+        nettobtr,
+        stprz,
+        hvorg,
+        tvorg,
+        txjcd,
+        xtotal_amnt
+    from
+        {{ ref('stg_dberdl') }}
+),
+
+itens_faturamento_impostos_base as (
+    select
+        df.mes_competencia,
+        df.doc_calculo as documento_calculo,
+        df.doc_impressao as documento_impressao,
+        d.belzart,
+        d.linesort,
+        d.ab,
+        d.bis,
+        d.sbasw,
+        df.estorno,
+        d.ktosl,
+        d.nettobtr,
+        d.stprz,
+        d.hvorg,
+        d.tvorg,
+        d.txjcd
+    from
+        documentos_faturamento as df
+    left outer join
+        dberdl as d
+        on df.doc_impressao = d.printdoc
+    where
+        d.xtotal_amnt <> 'X'
+        and d.linesort in (
+            'ZDR1', 'ZDR2', 'ZDR3', 'ZDR4', 'ZDR5',
+            'ZDR6', 'ZUR1', 'ZUR2', 'ZUR3', 'ZDES'
+        )
+),
+
+itens_faturamento_impostos_transformados as (
+    select
+        mes_competencia,
+        documento_calculo,
+        documento_impressao,
+        null as linha,
+        null as setor_industrial,
+        null as categoria_tarifa,
+        null as subclasse,
+        belzart as item_documento,
+        '000' as escalao,
+        sbasw as base_imposto,
+        'D' as flag,
+        estorno,
+        case
+            when linesort = ' ' then null
+            else linesort
+        end as item_ordenacao,
+        case
+            when ab <> '00000000' then TO_DATE(ab, 'YYYYMMDD')
+        end as inicio_calculo,
+        case
+            when bis <> '00000000' then TO_DATE(bis, 'YYYYMMDD')
+        end as fim_calculo,
+        case
+            when ktosl = ' ' then null
+            else ktosl
+        end as tipo_imposto,
+        CAST(null as NUMBER) as consumo,
+        CAST(null as NUMBER) as preco,
+        case
+            when estorno = 'x' then nettobtr * -1
+            else nettobtr
+        end as receita,
+        case
+            when stprz = ' ' then 0
+            else TRY_CAST(LTRIM(stprz, ' ') as NUMBER(10, 3)) / 1000
+        end as aliquota,
+        case
+            when hvorg = ' ' then null
+            else hvorg
+        end as operacao,
+        case
+            when tvorg = ' ' then null
+            else tvorg
+        end as sub_operacao,
+        case
+            when txjcd = ' ' then null
+            else txjcd
+        end as domicilio_fiscal
+    from
+        itens_faturamento_impostos_base
+),
+
+final as (
+    select
+        mes_competencia,
+        documento_calculo,
+        documento_impressao,
+        linha,
+        setor_industrial,
+        categoria_tarifa,
+        subclasse,
+        item_documento,
+        escalao,
+        base_imposto,
+        flag,
+        estorno,
+        item_ordenacao,
+        inicio_calculo,
+        fim_calculo,
+        tipo_imposto,
+        consumo,
+        preco,
+        receita,
+        aliquota,
+        operacao,
+        sub_operacao,
+        domicilio_fiscal
+    from
+        itens_faturamento_impostos_transformados
+)
+
+select * from final
