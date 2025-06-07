469fe07502f729e5e9049c1d905d34b625494cb6
Gabriel Terceiro <gcarolinoterceiro@gmail.com>
2025-06-06 22:31:02 -0300

    Refactor: Improve int_documentos_faturamento model
    Removes final_merge.sql and improves int_documentos_faturamento.sql.
    Uses CTEs for readability and adds stg_int_ultima_exec logic directly.
    Also adds pre_hooks to int_ultima_execucao.sql.

--- models/intermediate/int_documentos_faturamento.sql
+++ models/intermediate/int_documentos_faturamento.sql
@@ -1,4 +1,43 @@
-with calculos as (
+with erdk as (
+    select
+        mandt,
+        opbel,
+        intopbel,
+        ergrd,
+        invoiced,
+        budat,
+        erdat
+    from
+        {{ ref('stg_erdk') }}
+),
+
+erchc as (
+    select
+        mandt,
+        opbel,
+        belnr
+    from
+        {{ ref('stg_erchc') }}
+),
+
+erch as (
+    select
+        mandt,
+        belnr,
+        belegart,
+        zzorigdoc
+    from
+        {{ ref('stg_erch') }}
+),
+
+ultima_execucao as (
+    select
+        ultima_execucao
+    from
+        {{ ref('stg_int_ultima_exec') }}
+),
+
+calculos as (
     select
         a.mandt,
         b.belnr as doc_calculo,
@@ -11,22 +50,19 @@ with calculos as (
             when a.ergrd = '04' then 'X'
         end as estorno,
         case
-            when a.budat <> '00000000' then SUBSTR(a.budat, 0, 6)
+            when a.budat <> '00000000' then SUBSTR(a.budat, 1, 6)
         end as mes_competencia
     from
-        {{ ref('stg_erdk') }} as a
+        erdk as a
     inner join
-        {{ ref('stg_erchc') }} as b
-        on (a.mandt = b.mandt) and (a.opbel = b.opbel)
+        erchc as b on a.mandt = b.mandt and a.opbel = b.opbel
     inner join
-        {{ ref('stg_erch') }} as c
-        on (b.mandt = c.mandt) and (b.belnr = c.belnr)
+        erch as c on b.mandt = c.mandt and b.belnr = c.belnr
     where
-        a.mandt = {{ mc_mandante(var('source_param')) }} 
+        a.mandt = {{ mc_mandante(var('source_param')) }}
         and a.invoiced = 'X'
         and a.ergrd <> '04'
-        and a.erdat
-        >= (select ultima_execucao from {{ ref('stg_int_ultima_exec') }})
+        and a.erdat >= (select ultima_execucao from ultima_execucao)
         and a.erdat <= TO_CHAR(CURRENT_DATE(), 'YYYYMMDD')
 ),
 
@@ -43,35 +79,30 @@ estornos_plenos as (
             when a.ergrd = '04' then 'X'
         end as estorno,
         case
-            when a.budat <> '00000000' then SUBSTR(a.budat, 0, 6)
+            when a.budat <> '00000000' then SUBSTR(a.budat, 1, 6)
         end as mes_competencia
     from
-        {{ ref ('stg_erdk') }} as a
-    inner join {{ ref ('stg_erchc') }} as b
-        on
-            (a.mandt = b.mandt)
-            and (a.intopbel = b.opbel)
-    inner join {{ ref ('stg_erch') }} as c
-        on
-            (b.mandt = c.mandt)
-            and b.belnr = c.belnr
+        erdk as a
+    inner join
+        erchc as b on a.mandt = b.mandt and a.intopbel = b.opbel
+    inner join
+        erch as c on b.mandt = c.mandt and b.belnr = c.belnr
     where
         a.mandt = {{ mc_mandante(var('source_param')) }}
         and a.invoiced = 'X'
         and a.ergrd = '04'
-        and a.erdat
-        >= (select ultima_execucao from {{ ref('stg_int_ultima_exec') }})
+        and a.erdat >= (select ultima_execucao from ultima_execucao)
         and a.erdat <= TO_CHAR(CURRENT_DATE(), 'YYYYMMDD')
 ),
 
-uniao as (
+documentos_unificados as (
     select
         mandt,
         doc_calculo,
         doc_impressao,
         tipo_calculo,
         tipo_documento,
-        TO_VARCHAR(estorno) as estorno,
+        estorno,
         mes_competencia
     from
         calculos
@@ -82,19 +113,23 @@ uniao as (
         doc_impressao,
         tipo_calculo,
         tipo_documento,
-        TO_VARCHAR(estorno) as estorno,
+        estorno,
         mes_competencia
     from
         estornos_plenos
+),
+
+final as (
+    select
+        mandt,
+        doc_calculo,
+        doc_impressao,
+        tipo_calculo,
+        tipo_documento,
+        estorno,
+        mes_competencia
+    from
+        documentos_unificados
 )
 
-select
-    mandt,
-    doc_calculo,
-    doc_impressao,
-    tipo_calculo,
-    tipo_documento,
-    estorno,
-    mes_competencia
-from
-    uniao
+select * from final
--- models/intermediate/int_ultima_execucao.sql
+++ models/intermediate/int_ultima_execucao.sql
@@ -1,3 +1,12 @@
---depends_on: {{ ref('faturamento') }}
+-- depends_on: {{ ref('int_docs_dups_ord_fat') }}
+
+{{
+  config(
+    pre_hook = [
+      "{{ atualiza_ordem() }}",
+      "{{ vlr_cont_delta() }}"
+    ]
+  )
+}}
 
 select TO_CHAR(CURRENT_DATE(), 'YYYYMMDD') as ultima_execucao
