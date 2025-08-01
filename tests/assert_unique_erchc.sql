3309dad038476a382160d93b3a2ea829403cd282
Gabriel Terceiro <gcarolinoterceiro@gmail.com>
2025-08-01 15:51:20 -0300

    Refactor: Remove null tests from intermediate and marts
    Removes not_null tests from intermediate and marts yml files.
    These tests were redundant and were impacting performance.
    Also removed tests that will be implemented in other forms.

--- tests/assert_unique_erchc.sql
+++ tests/assert_unique_erchc.sql
@@ -1,10 +1,12 @@
 select
     opbel as unique_field_0,
     belnr as unique_field_1,
+    lfdnr as unique_field_2,
     count(*) as n_records
 from {{ source('RAW', 'ERCHC') }}
 where 
     opbel is not null
     and belnr is not null
-group by opbel, belnr
+    and lfdnr is not null
+group by all
 having count(*) > 1
