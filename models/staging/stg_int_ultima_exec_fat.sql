ac191ba870b8c315f3afcd7dce3edf758ac31d89
Gabriel Terceiro <gcarolinoterceiro@gmail.com>
2025-08-19 10:17:49 -0300

    Refactor: Simplify stg_int_ultima_exec_fat.sql
    Removes unnecessary logic and simplifies the staging model.
    This improves readability and maintainability by directly
    querying the `ultima_carga` column. Also, delete the
    `int_ultima_execucao` model that is no longer needed.

--- models/staging/stg_int_ultima_exec_fat.sql
+++ models/staging/stg_int_ultima_exec_fat.sql
@@ -1,6 +1,4 @@
-{# select TO_CHAR(ultima_carga, 'YYYYMMDD') as ultima_execucao
+select TO_CHAR(ultima_carga, 'YYYYMMDD') as ultima_execucao
 from
     {{ source('PROD','TAB_CONTROLE_CARGAS') }}
-where tabela = 'FATURAMENTO' #}
-
-select '20250701' as ultima_execucao
+where tabela = 'FATURAMENTO'
