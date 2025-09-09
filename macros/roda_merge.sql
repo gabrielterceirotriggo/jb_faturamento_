1082d272c75efa3d23b400cc2c0656c0319bca7d
Gabriel Terceiro <gcarolinoterceiro@gmail.com>
2025-09-09 17:35:14 -0300

    Refactor: Run stored proc conditionally on source
    Run stored procedures only if the source parameter matches
    EQTL_MA, EQTL_PA, EQTL_PI, or EQTL_AL. This prevents errors
    when no stored procedure exists for the given source.

--- macros/roda_merge.sql
+++ macros/roda_merge.sql
@@ -3,7 +3,10 @@
         CALL {{var('database_raw')}}.{{var('source_orig')}}.PRC_{{tabela}}_DELTA({{get_days_since()}});
     {% endset %}
 
-    {% do run_query(commands) %}
-
-    {{ log("Procedure PRC_" ~ tabela ~ "_DELTA rodou com sucesso para " ~ get_days_since() ~ " dias", info=True) }}
+    {% if var('source_param') == 'EQTL_MA' or var('source_param') == 'EQTL_PA' or var('source_param') == 'EQTL_PI' or var('source_param') == 'EQTL_AL' %}
+        {% do run_query(commands) %}
+        {{ log("Procedure PRC_" ~ tabela ~ "_DELTA rodou com sucesso para " ~ get_days_since() ~ " dias", info=True) }}
+    {% else %}
+        {{ log("Não há procedure para esta empresa.", info=True) }}
+    {% endif %}
 {% endmacro %}
